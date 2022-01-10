fe041aff1da0ec0fea36acac1108f7793f1aca87
Santhoshgv1 <47055747+Santhoshgv1@users.noreply.github.com>
2022-01-10 11:28:16 +0530

    Update Std.java

--- newdemo/src/com/demo/Std.java
+++ newdemo/src/com/demo/Std.java
@@ -5,6 +5,8 @@ public class Std {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		System.out.println("chnage from master");		
+		
 	}
 
 }
