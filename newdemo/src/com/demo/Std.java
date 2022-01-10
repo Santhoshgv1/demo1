9ac0043f3ec67fd7303c529f26255e77b3746c19
Santhoshgv1 <santhoshgv0@gmail.com>
2022-01-10 11:54:15 +0530

    new commit from eclipse

--- newdemo/src/com/demo/Std.java
+++ newdemo/src/com/demo/Std.java
@@ -3,9 +3,7 @@ package com.demo;
 public class Std {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		System.out.println("chnage from master");
+
 		System.out.println("hello santhosh");
 	}
-
 }
