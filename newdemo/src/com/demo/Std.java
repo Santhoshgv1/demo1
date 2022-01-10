1d181cad047c4919315c695c3d250cdb5fa25156
Santhoshgv1 <47055747+Santhoshgv1@users.noreply.github.com>
2022-01-10 11:49:24 +0530

    new update

--- newdemo/src/com/demo/Std.java
+++ newdemo/src/com/demo/Std.java
@@ -5,8 +5,7 @@ public class Std {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("chnage from master");
-		System.out.println("hello");
-		
+		System.out.println("hello santhosh");
 	}
 
 }
