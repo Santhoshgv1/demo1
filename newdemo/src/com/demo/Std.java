c5b5e9e4d3da537750a18c59b42539822e6d7270
Santhoshgv1 <47055747+Santhoshgv1@users.noreply.github.com>
2022-01-10 11:46:56 +0530

    Update Std.java
    demo

--- newdemo/src/com/demo/Std.java
+++ newdemo/src/com/demo/Std.java
@@ -4,7 +4,9 @@ public class Std {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("chnage from master");		
+		System.out.println("chnage from master");
+		System.out.println("hello");
+		
 	}
 
 }
